3e824087729a6d33a553f4dd07004a0e3c93ea58
kent-toon-lau <kent.toon.lau@beier360.com>
2017-06-21 15:23:20 +0100

    Inital Build

---
+++ Starter_App/Starter_App/Starter_App/MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Starter_App
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+
+        private void Add_Clicked(object sender, EventArgs e)
+        {
+            ResultLabel.Text = (Convert.ToInt32(ValueAEntry.Text) + Convert.ToInt32(ValueBEntry.Text)).ToString();
+        }
+
+        private void Multiply_Clicked(object sender, EventArgs e)
+        {
+            ResultLabel.Text = (Convert.ToInt32(ValueAEntry.Text) * Convert.ToInt32(ValueBEntry.Text)).ToString();
+        }
+    }
+}
